4a73d545c6d321d9c67261dd538d192b35211e0d
Arsalan Yaldram <arsalanyaldram0211@outlook.com>
2023-08-16 15:06:00 +0530

    feat: setup of panda-css completed.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { css } from "../styled-system/css";
+
+export function App() {
+  return (
+    <div className={css({ pl: "96", fontSize: "2xl", fontWeight: "bold" })}>
+      Hello 🐼!
+    </div>
+  );
+}
